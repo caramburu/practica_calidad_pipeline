0b1d8f542d3dbe240c0cd24bd24bef716ae486e0
caramburu <claudio.aramburu@upn.pe>
2026-02-21 17:15:49 -0500

    Change sum calculation from 5 + 2 to 5 + 5

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ console.log("Iniciando sistema de pruebas de Calidad...");
 function sumar(a, b) { 
     return a + b; 
 } 
-const resultado = sumar(5, 2); 
+const resultado = sumar(5, 5); 
 console.log("Resultado del cálculo de prueba (5+5): " + resultado); 
 
 if (resultado === 10) { 
